3bcb68bee152546d195c0c49128529d58c0c2451
Asad1024 <asadshah1024@gmail.com>
2025-12-24 13:34:57 +0500

    chore: normalize Hero image filenames

--- components/Hero.js
+++ components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero(){
         <div className="container mx-auto px-6">
           <div className="flex flex-col items-center">
               <div className="mb-14 w-21 h-20" style={{ marginRight: '20px' }}>
-              <Image src="/images/setapp-icon-birthday-15.svg.png" alt="setapp" width={128} height={128} />
+              <Image src="/images/setapp-icon-birthday-15.png" alt="setapp" width={128} height={128} />
             </div>
 
             <h1 className="hero-title font-extrabold text-center max-w-2xl mx-auto mb-3" style={{ fontFamily: 'Avenir Next, system-ui, sans-serif', fontWeight: 700, fontSize: '64px', lineHeight: '67.84px', letterSpacing: '1px' }}>Dozens of apps.<br/>One subscription.</h1>
@@ -18,10 +18,10 @@ export default function Hero(){
             <div className="flex items-center justify-center gap-3 mb-4">
               <button className="bg-white text-black px-8 py-3 rounded-lg text-sm font-medium hover:bg-gray-100 transition">Try free for 7 days</button>
               <button className="w-11 h-11 bg-white flex items-center justify-center rounded-lg overflow-hidden hover:bg-gray-100 transition">
-                <Image src="/images/Link (1).png" alt="apple" width={32} height={32} className="object-contain" />
+                <Image src="/images/app-store-badge.png" alt="apple" width={32} height={32} className="object-contain" />
               </button>
               <button className="w-11 h-11 bg-white flex items-center justify-center rounded-lg overflow-hidden hover:bg-gray-100 transition">
-                <Image src="/images/Link.png" alt="google" width={32} height={32} className="object-contain" />
+                <Image src="/images/google-play-badge.png" alt="google" width={32} height={32} className="object-contain" />
               </button>
             </div>
 
@@ -33,36 +33,36 @@ export default function Hero(){
       {/* Decorative stickers positioned to match Figma */}
       {/* Left side stickers */}
       <div className="absolute left-[8%] top-[15%] -rotate-10 z-[13]">
-        <Image src="/images/pics.svg.png" alt="pics" width={173} height={173} />
+        <Image src="/images/pics-sticker.png" alt="pics" width={173} height={173} />
       </div>
       <div className="absolute left-[8%] top-[34%] rotate-3 z-[15]">
-        <Image src="/images/Group.png" alt="macpaw" width={58} height={58} />
+        <Image src="/images/group-sticker.png" alt="macpaw" width={58} height={58} />
       </div>
       <div className="absolute left-[15%] top-[32%] -rotate-6 z-[14]">
-        <Image src="/images/pdf.svg.png" alt="pdf" width={140} height={140} />
+        <Image src="/images/pdf-sticker.png" alt="pdf" width={140} height={140} />
       </div>
       <div className="absolute left-[6%] top-[55%] -rotate-3 z-[11]">
-        <Image src="/images/teamwork.svg.png" alt="teamwork" width={226} height={123} />
+        <Image src="/images/teamwork-sticker.png" alt="teamwork" width={226} height={123} />
       </div>
       <div className="absolute left-[6%] top-[68%] rotate-3 z-[12]">
-        <Image src="/images/secure.svg.png" alt="secure" width={175} height={176} />
+        <Image src="/images/secure-sticker.png" alt="secure" width={175} height={176} />
       </div>
 
       {/* Right side stickers */}
       <div className="absolute right-[9%] top-[13%] rotate-10 z-[13]">
-        <Image src="/images/wifi.svg.png" alt="wifi" width={200} height={125} />
+        <Image src="/images/wifi-sticker.png" alt="wifi" width={200} height={125} />
       </div>
       <div className="absolute right-[16%] top-[22%] -rotate-6 z-[14]">
-        <Image src="/images/plan.svg.png" alt="plan" width={169} height={169} />
+        <Image src="/images/plan-sticker.png" alt="plan" width={169} height={169} />
       </div>
       <div className="absolute right-[7%] top-[38%] rotate-12 z-[15]">
-        <Image src="/images/converter.svg.png" alt="converter" width={155} height={153} />
+        <Image src="/images/converter-sticker.png" alt="converter" width={155} height={153} />
       </div>
       <div className="absolute right-[11%] top-[53%] -rotate-7 z-[11]">
-        <Image src="/images/manage.svg.png" alt="manage" width={230} height={128} />
+        <Image src="/images/manage-sticker.png" alt="manage" width={230} height={128} />
       </div>
       <div className="absolute right-[12%] top-[67%] rotate-9 z-[12]">
-        <Image src="/images/code.svg.png" alt="code" width={152} height={153} />
+        <Image src="/images/code-sticker.png" alt="code" width={152} height={153} />
       </div>
     </section>
     
